d5b2d9d42431638c0b83ee186c98f96b9385ec6c
Bao Trong <nhbaotrong@gmail.com>
2026-03-21 19:44:04 +0700

    first commit

---
+++ app/(tabs)/apartment.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function ApartmentScreen() {
+     return (
+          <View>
+               <Text>apartment</Text>
+          </View>
+     )
+}
---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Link, useRouter } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+export default function Index() {
+  const router = useRouter()
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="TAB" onPress={() => router.navigate('/(tabs)/home')} />
+      <Button title="LOGIN SCREEN" onPress={() => router.navigate('/login')} />
+    </View>
+  );
+}
